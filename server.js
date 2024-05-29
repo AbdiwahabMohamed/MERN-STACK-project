759b98a369d4bf06aee108082c18bed3f0277cc4
Abdiwahab Mohamed <Fahadfar69@gmail.com>
2024-05-29 11:48:13 +0300

    Finnishing The Backend

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ import express from "express";
 import connectedToDb from "./src/config/connectedToDb.js";
 import dotenv from "dotenv";
 import { notFound, errorHandler } from "./src/middleware/error.js";
+import authRoute from "./src/routes/authRoute.js";
+import usersRoute from "./src/routes/userRoute.js";
+import postsRoute from "./src/routes/postsRoute.js";
+import commentsRoute from "./src/routes/commentsRoute.js";
+import categoriesRoute from "./src/routes/categoriesRoute.js";
 
 dotenv.config();
 
@@ -13,12 +18,19 @@ const server = express();
 
 server.use(express.json());
 
-const PORT = process.env.PORT || 3000;
+// Routes
+app.use("/api/auth", authRoute);
+app.use("/api/users", usersRoute);
+app.use("/api/posts", postsRoute);
+app.use("/api/comments", commentsRoute);
+app.use("/api/categories", categoriesRoute);
 
 // Error Handler Middleware
 app.use(notFound);
 app.use(errorHandler);
 
+// Running The Server
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
